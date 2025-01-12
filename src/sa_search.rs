8a9c8d4735357eccd4c8c6cd55360f25def07881
Matthew Martin <matthew@mttmartin.com>
2025-01-12 18:07:18 +0100

    update for benchmarking

--- src/sa_search.rs
+++ src/sa_search.rs
@@ -6,6 +6,10 @@ use std::io::BufReader;
 use std::path::PathBuf;
 use std::str;
 use clap::Parser;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::time::{SystemTime, UNIX_EPOCH};
+use crate::fasta::Record;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about= None)]
@@ -13,7 +17,9 @@ struct Args {
     /// Path to reads in a gzipped fasta file 
     reads: PathBuf,
     /// Path to references in a gzipped fasta file
-    references: PathBuf
+    references: PathBuf,
+    /// Number of reads to use
+    read_num: usize
 }
 
 fn naive_binary_search(reference: &str, read: &str, sa: &Vec<usize>) -> (i32, i32) {
@@ -48,14 +54,38 @@ fn naive_binary_search(reference: &str, read: &str, sa: &Vec<usize>) -> (i32, i3
     (first, last)
 }
 
+fn get_reads(reads_path: &PathBuf) -> Vec<Record> {
+    let reads_reader = BufReader::new(GzDecoder::new(File::open(reads_path.clone()).unwrap()));
+    let reads = fasta::Reader::new(reads_reader);
+    let mut reads_vec: Vec<Record> = Vec::new();
+    for read in reads.records() {
+        let read = read.expect("error fetching read");
+        reads_vec.push(read);
+    }
+    reads_vec
+}
+
 fn main() {
     let args = Args::parse();
 
     let reads_path = args.reads;
+    let mut reads_vec: Vec<Record> = get_reads(&reads_path);
+
+    while reads_vec.len() < args.read_num {
+        let new_reads = get_reads(&reads_path);
+        reads_vec.extend(new_reads);
+    }
+    reads_vec.truncate(args.read_num);
+
     let references_path = args.references;
 
     let references_reader = BufReader::new(GzDecoder::new(File::open(references_path).unwrap()));
     let references = fasta::Reader::new(references_reader);
+
+    let mut benchmark_file = OpenOptions::new().create(true).append(true).open("rs_benchmark.csv").expect("error opening benchmark file");
+    if benchmark_file.metadata().expect("error opening file metadata").len() == 0 {
+        writeln!(benchmark_file, "method,reads_file,time,read_n");
+    }
 
     for reference in references.records() {
         let reference = str::from_utf8(reference.expect("Error reading reference sequence").seq())
@@ -63,11 +93,12 @@ fn main() {
             .to_string()
             + "$";
         let sa = suffix_array(reference.as_bytes());
+        let start_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
 
         let reads_reader = BufReader::new(GzDecoder::new(File::open(reads_path.clone()).unwrap()));
         let reads = fasta::Reader::new(reads_reader);
-        for read in reads.records() {
-            let read = read.expect("error fetching read");
+        let mut read_num = 0;
+        for read in &reads_vec {
             let (first, last) =
                 naive_binary_search(&reference, str::from_utf8(read.seq()).unwrap(), &sa);
             if first > 0 {
@@ -75,6 +106,11 @@ fn main() {
                     println!("{}", str::from_utf8(read.seq()).unwrap());
                 }
             }
+
+            if read_num % 10 == 0 {
+                writeln!(benchmark_file, "sa,{:?},{},{}", reads_path, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() - start_time, read_num);
+            }
+            read_num += 1;
         }
     }
 }
